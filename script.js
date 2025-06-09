8c33a971a7d9921dd67d874d8524e699704cd75a
Jose Oña <monal@est.ups.edu.ec>
2025-06-09 16:44:11 -0500

    Variables let concatenar

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 function mostrarMensaje(){
     alert("Hola mundo :) ");
 }
+
+
+// Variables
+function imprimirVariables(){
+    let nombre = "Rodrigo";
+    let edad = 42;
+    // Formato condicional de concatenar cadenas
+    console.log("nombre: ", nombre)
+    console.log("edad: ", edad)
+    //Forma moderna de concatenar cadenas 
+    console.log(`nombre:${nombre}, edad: ${edad}`)
+    // otros tipos de impresion
+    console.error("Error: 12345 ");
+    console.warn("Advertencia: x x x");
+}
+
